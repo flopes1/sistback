2f3c2c8eb0f87558078b7c8d89c01d017333e4f4
Filipe Lopes <flm@ecomp.poli.br>
2017-03-06 00:24:23 -0300

    Adiciona properties

--- ssl-config/src/main/java/com/sist/ssl/loader/SslSetupConstants.java
+++ ssl-config/src/main/java/com/sist/ssl/loader/SslSetupConstants.java
@@ -2,5 +2,19 @@ package com.sist.ssl.loader;
 
 public interface SslSetupConstants
 {
+    // Data Base Properties
+    public static final String DATA_BASE_DRIVER = "db.driver";
+    public static final String DATA_BASE_URL = "db.url";
+    public static final String DATA_BASE_USERNAME = "db.username";
+    public static final String DATA_BASE_PASSWORD = "db.password";
+    public static final String DATA_BASE_SCHEMA = "db.schema";
 
+    //Hibernate Properties
+    public static final String HIBERNATE_DIALECT = "hibernate.dialect";
+    public static final String HIBERNATE_HBM2DDL_VALIDATE = "hibernate.hbm2ddl.auto";
+    public static final String HIBERNATE_SHOW_SQL = "hibernate.show.sql";
+    public static final String HIBERNATE_FORMAT_SQL = "hibernate.format.sql";
+    public static final String HIBERNATE_BATCH_SIZE = "hibernate.batch.size";
+    public static final String HIBERNATE_CURRENT_SESSION_CONTEXT_CLASS = "hibernate.current.session.context.class";
+    public static final String HIBERNATE_AUTO_REGISTER_LISTENERS = "hibernate.search.autoregister.listeners";
 }
