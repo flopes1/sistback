7b26782075b014728858b8489831d6091453f1d4
Filipe Lopes <flm@ecomp.poli.br>
2017-03-06 00:24:33 -0300

    Corrige script do db

--- ssl-database-postgresql/src/main/resources/scripts/v1.0.0.1.1__create_tables.sql
+++ ssl-database-postgresql/src/main/resources/scripts/v1.0.0.1.1__create_tables.sql
@@ -1,4 +1,4 @@
-CREATE TABLE ssl.condominium 
+CREATE TABLE dbo.condominium 
 (
 	id SERIAL,
 	name TEXT NOT NULL,
@@ -6,7 +6,7 @@ CREATE TABLE ssl.condominium
 	PRIMARY KEY (id)
 );
 
-CREATE TABLE ssl.employee 
+CREATE TABLE dbo.employee 
 (
 	id SERIAL,
 	condominium_id INTEGER,
@@ -15,11 +15,11 @@ CREATE TABLE ssl.employee
 	adm BOOLEAN DEFAULT FALSE,
 	email TEXT NOT NULL,
 	PRIMARY KEY (id),
-	FOREIGN KEY (condominium_id) REFERENCES ssl.condominium (id) 
+	FOREIGN KEY (condominium_id) REFERENCES dbo.condominium (id) 
 	ON DELETE CASCADE
 );
 
-CREATE TABLE ssl.userr
+CREATE TABLE dbo.userr
 (
 	id SERIAL,
 	cpf INTEGER NOT NULL UNIQUE,
@@ -27,21 +27,21 @@ CREATE TABLE ssl.userr
 	apartment_identifier TEXT NOT NULL,
 	condominium_id INTEGER,
 	PRIMARY KEY (id),
-	FOREIGN KEY (condominium_id) REFERENCES ssl.condominium (id)
+	FOREIGN KEY (condominium_id) REFERENCES dbo.condominium (id)
 	ON DELETE CASCADE 
 );
 
-CREATE TABLE ssl.visitor
+CREATE TABLE dbo.visitor
 (
 	id SERIAL,
 	condominium_id INTEGER,
 	name TEXT NOT NULL,
 	PRIMARY KEY (id),
-	FOREIGN KEY (condominium_id) REFERENCES ssl.condominium (id)
+	FOREIGN KEY (condominium_id) REFERENCES dbo.condominium (id)
 	ON DELETE CASCADE
 );
 
-CREATE TABLE ssl.condominium_address 
+CREATE TABLE dbo.condominium_address 
 (
 	id SERIAL,
 	condominium_id INTEGER,
@@ -52,114 +52,114 @@ CREATE TABLE ssl.condominium_address
 	city TEXT NOT NULL,
 	zip_code INTEGER NOT NULL,
 	country TEXT NOT NULL,
-	PRIMARY KEY (id, condominium_id),
-	FOREIGN KEY (condominium_id) REFERENCES ssl.condominium (id)
+	PRIMARY KEY (id),
+	FOREIGN KEY (condominium_id) REFERENCES dbo.condominium (id)
 	ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE TABLE ssl.phone_number_condominium
+CREATE TABLE dbo.phone_number_condominium
 (
 	id SERIAL,
 	condominium_id INTEGER,
 	country_code SMALLINT NOT NULL,
 	state_code SMALLINT NOT NULL,
 	number_ INTEGER NOT NULL,
-	PRIMARY KEY (id, condominium_id),
-	FOREIGN KEY (condominium_id) REFERENCES ssl.condominium (id)
+	PRIMARY KEY (id),
+	FOREIGN KEY (condominium_id) REFERENCES dbo.condominium (id)
 	ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE TABLE ssl.phone_number_userr
+CREATE TABLE dbo.phone_number_userr
 (
 	id SERIAL,
 	userr_id INTEGER,
 	country_code SMALLINT NOT NULL,
 	state_code SMALLINT NOT NULL,
 	number_ INTEGER NOT NULL,
-	PRIMARY KEY (id, userr_id),
-	FOREIGN KEY (userr_id) REFERENCES ssl.userr (id)
+	PRIMARY KEY (id),
+	FOREIGN KEY (userr_id) REFERENCES dbo.userr (id)
 	ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE TABLE ssl.phone_number_employee
+CREATE TABLE dbo.phone_number_employee
 (
 	id SERIAL,
 	employee_id INTEGER,
 	country_code SMALLINT NOT NULL,
 	state_code SMALLINT NOT NULL,
 	number_ INTEGER NOT NULL,
-	PRIMARY KEY (id, employee_id),
-	FOREIGN KEY (employee_id) REFERENCES ssl.employee (id)
+	PRIMARY KEY (id),
+	FOREIGN KEY (employee_id) REFERENCES dbo.employee (id)
 	ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE TABLE ssl.phone_number_visitor
+CREATE TABLE dbo.phone_number_visitor
 (
 	id SERIAL,
 	visitor_id INTEGER,
 	country_code SMALLINT NOT NULL,
 	state_code SMALLINT NOT NULL,
 	number_ INTEGER NOT NULL,
-	PRIMARY KEY (id, visitor_id),
-	FOREIGN KEY (visitor_id) REFERENCES ssl.visitor (id)
+	PRIMARY KEY (id),
+	FOREIGN KEY (visitor_id) REFERENCES dbo.visitor (id)
 	ON DELETE CASCADE
 );
 
-CREATE TABLE ssl.vehicle
+CREATE TABLE dbo.vehicle
 (
 	id SERIAL,
 	userr_id INTEGER,
 	model TEXT NOT NULL,
 	color TEXT NOT NULL,
 	license_plate TEXT NOT NULL,
-	PRIMARY KEY (id, userr_id),
-	FOREIGN KEY (userr_id) REFERENCES ssl.userr (id)
+	PRIMARY KEY (id),
+	FOREIGN KEY (userr_id) REFERENCES dbo.userr (id)
 	ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE TABLE ssl.enum_status_kind 
+CREATE TABLE dbo.enum_status_kind 
 (
 	id SERIAL,
 	name TEXT NOT NULL UNIQUE,
 	PRIMARY KEY (id)
 );
 
-INSERT INTO ssl.enum_status_kind (name) VALUES ('Finalizado');
-INSERT INTO ssl.enum_status_kind (name) VALUES ('Cancelado');
-INSERT INTO ssl.enum_status_kind (name) VALUES ('Em aberto');
+INSERT INTO dbo.enum_status_kind (name) VALUES ('Finalizado');
+INSERT INTO dbo.enum_status_kind (name) VALUES ('Cancelado');
+INSERT INTO dbo.enum_status_kind (name) VALUES ('Em aberto');
 
-CREATE TABLE ssl.autentication_history
+CREATE TABLE dbo.autentication_history
 (
 	id SERIAL,
-	condominium_id INTEGER REFERENCES ssl.condominium (id) ON DELETE CASCADE,
-	userr_id INTEGER REFERENCES ssl.userr (id) ON DELETE CASCADE,
-	employee_id INTEGER REFERENCES ssl.employee (id) ON DELETE CASCADE,
+	condominium_id INTEGER REFERENCES dbo.condominium (id) ON DELETE CASCADE,
+	userr_id INTEGER REFERENCES dbo.userr (id) ON DELETE CASCADE,
+	employee_id INTEGER REFERENCES dbo.employee (id) ON DELETE CASCADE,
 	accepted BOOLEAN DEFAULT FALSE,
 	reason TEXT NULL,
 	date_ TIMESTAMP NOT NULL,
-	PRIMARY KEY (id, condominium_id, userr_id, employee_id)
+	PRIMARY KEY (id)
 );
 
-CREATE TABLE ssl.scheduled_visit
+CREATE TABLE dbo.scheduled_visit
 (
 	id SERIAL NOT NULL UNIQUE,
-	condominium_id INTEGER REFERENCES ssl.condominium (id) ON DELETE CASCADE,
-	userr_requester_id INTEGER REFERENCES ssl.userr (id) ON DELETE CASCADE,
-	visitor_id INTEGER REFERENCES ssl.visitor (id) ON DELETE CASCADE,
+	condominium_id INTEGER REFERENCES dbo.condominium (id) ON DELETE CASCADE,
+	userr_requester_id INTEGER REFERENCES dbo.userr (id) ON DELETE CASCADE,
+	visitor_id INTEGER REFERENCES dbo.visitor (id) ON DELETE CASCADE,
 	visit_date TIMESTAMP NOT NULL,
 	kind TEXT NULL,
 	has_vehicle BOOLEAN NULL,
-	status INTEGER NULL REFERENCES ssl.enum_status_kind, 
-	PRIMARY KEY (id, condominium_id, userr_requester_id, visitor_id)
+	status INTEGER NULL REFERENCES dbo.enum_status_kind, 
+	PRIMARY KEY (id)
 );
 
-CREATE TABLE ssl.service_provision
+CREATE TABLE dbo.service_provision
 (
 	id SERIAL,
 	scheduled_visit_id INTEGER,
 	company TEXT NOT NULL,
 	description TEXT NULL,
-	PRIMARY KEY (id, scheduled_visit_id),
-	FOREIGN KEY (scheduled_visit_id) REFERENCES ssl.scheduled_visit (id)
+	PRIMARY KEY (id),
+	FOREIGN KEY (scheduled_visit_id) REFERENCES dbo.scheduled_visit (id)
 	ON DELETE CASCADE
 );
